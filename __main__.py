d1646477f407b4dd0f7824d5afb8c670699f6e77
aliaa80 <a.ansari80@outlook.com>
2020-04-20 12:02:37 +0430

    Add start function

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from Game import Game
+
 updater = Updater(token='1212931959:AAHH9ViQhhhVRJBsEs9EwBv2pfkg8BMDFS4', use_context=True)
 
 
@@ -11,17 +12,20 @@ def new_game(update: telegram.Update, context: telegram.ext.CallbackContext):
     has_active_game = False
     print(group_id)
     for game in games:
-        if(game.group_chat_id==group_id):
-            has_active_game=True
-    if(not has_active_game):
+        if (game.group_chat_id == group_id):
+            has_active_game = True
+    if (not has_active_game):
         update.message.reply_text("New game started")
         game = Game(group_id)
         games.append(game)
+        context.job_queue.run_once(start_game, 30, context=update.message.chat_id)
     else:
         update.message.reply_text("The game has already started")
 
+
 def start(update: telegram.Update, context: telegram.ext.CallbackContext):
     update.message.reply_text("Hi!")
+
 
 def join(update: telegram.Update, context: telegram.ext.CallbackContext):
     global games
@@ -30,13 +34,14 @@ def join(update: telegram.Update, context: telegram.ext.CallbackContext):
     has_started_game = False
     group_game = None
     for game in games:
-        if(game.group_chat_id==group_id):
+        if (game.group_chat_id == group_id):
             has_started_game = True
             group_game = game
-    if(has_started_game):
-        group_game.join_game(user_info,update)
+    if (has_started_game):
+        group_game.join_game(user_info, update)
     else:
         update.message.reply_text("There is no game in this group!")
+
 
 def leave(update: telegram.Update, context: telegram.ext.CallbackContext):
     global games
@@ -49,9 +54,21 @@ def leave(update: telegram.Update, context: telegram.ext.CallbackContext):
             has_started_game = True
             group_game = game
     if (has_started_game):
-        group_game.leave_game(user_info,update)
+        group_game.leave_game(user_info, update)
     else:
         update.message.reply_text("There is no game in this group!")
+
+
+def start_game(context: telegram.ext.CallbackContext):
+    global games
+    context.bot.send_message(chat_id=context.job.context, text='Game has been started!')
+    group_id = context.job.context
+    for game in games:
+        if game.group_chat_id == group_id:
+            game.start_game()
+            for player in game.players:
+                context.bot.send_message(chat_id=player.user_id, text=player.rule)
+            break
 
 
 games = []
