3a5e6abf1961712d8b64457431a7110b76634eef
aliaa80 <a.ansari80@outlook.com>
2020-04-20 12:01:33 +0430

    Add user_id to this class to chat in player's pv

--- Player.py
+++ Player.py
@@ -1,6 +1,6 @@
 class Player:
-    def __init__(self, name,user_name):
-        self.rule = None
+    def __init__(self, name, user_name, user_id):
+        self.rule = "citizen"
         self.name = name
         self.user_name = user_name
-
+        self.user_id = user_id
