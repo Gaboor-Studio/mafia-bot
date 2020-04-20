7d4e772d4534c5b68d4d34ca988c5c76af3a2052
Amirparsa-Sal <Amirparsa.s@aut.ac.ir>
2020-04-21 04:14:22 +0430

    Prevent player to multiple games

--- Game.py
+++ Game.py
@@ -9,31 +9,31 @@ class Game:
         self.players = []
         self.group_chat_id = group_chat_id
 
-    def join_game(self, user_info: telegram.User, update: telegram.Update):
-        is_in_the_game = False
-        for player in self.players:
-            if player.user_name == user_info['username']:
-                is_in_the_game = True
-        if not is_in_the_game:
-            player = Player(user_info['first_name'], user_info['username'], user_info['id'])
+    def join_game(self, user: telegram.User, user_data: telegram.ext.CallbackContext.user_data,
+                  update: telegram.Update):
+        if "active_game" not in user_data.keys():
+            user_data["active_game"] = self
+            player = Player(user['first_name'], user['username'], user['id'])
             self.players.append(player)
-            players_list = ""
-            for player in self.players:
-                players_list += '@' + player.user_name + '\n'
-            update.message.reply_text(players_list)
-
+            update.message.reply_text(self.get_list())
         else:
-            update.message.reply_text('@' + user_info['username'] + " has already joined the game")
+            if user_data["active_game"] == self:
+                update.message.reply_text('@' + user['username'] + " has already joined the game!")
+            else:
+                update.message.reply_text('@' + user['username'] + " has already joined a game in another group!")
 
-    def leave_game(self, user_info: telegram.User, update: telegram.Update):
-        is_in_the_game = False
-        for player in self.players:
-            if player.user_name == user_info['username']:
+    def leave_game(self, user: telegram.User, user_data: telegram.ext.CallbackContext.user_data,
+                   update: telegram.Update):
+        if "active_game" in user_data.keys():
+            if user_data["active_game"] == self:
+                del user_data["active_game"]
+                player = self.get_player(user)
                 self.players.remove(player)
-                is_in_the_game = True
-                update.message.reply_text('@' + player.user_name + " successfully left the game")
-        if not is_in_the_game:
-            update.message.reply_text("You have not joined the game")
+                update.message.reply_text('@' + player.user_name + " successfully left the game!")
+            else:
+                update.message.reply_text("You are not in this game!")
+        else:
+            update.message.reply_text("You have not joined a game yet!")
 
     def start_game(self):
         for i in range(int(len(self.players) / 3)):
@@ -52,3 +52,15 @@ class Game:
             if self.players[r].rule == "citizen":
                 self.players[r].rule = "doctor"
                 break
+
+    def get_list(self):
+        players_list = ""
+        for player in self.players:
+            players_list += '@' + player.user_name + '\n'
+        return players_list
+
+    def get_player(self, user: telegram.User):
+        for player in self.players:
+            if player.user_name == user["username"]:
+                return player
+        return None
--- __main__.py
+++ __main__.py
@@ -3,18 +3,18 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from Game import Game
 
-updater = Updater(token='1212931959:AAHH9ViQhhhVRJBsEs9EwBv2pfkg8BMDFS4', use_context=True)
+updater = Updater(token='1292704420:AAF_EyffRm1uwKCwuZ8n6okijs1BY60S128', use_context=True)
+
 
 
 def new_game(update: telegram.Update, context: telegram.ext.CallbackContext):
     group_id = update.effective_chat.id
     group_data = context.chat_data
-    if("active_game" not in group_data.keys()):
-        update.message.reply_text("New game started")
+    if "active_game" not in group_data.keys():
         game = Game(group_id)
         group_data["active_game"] = game
-        games.append(game)
-        context.job_queue.run_once(start_game, 30, context=update.message.chat_id)
+        context.job_queue.run_once(start_game, 30, context=(update.message.chat_id,context.chat_data))
+        update.message.reply_text("New game started")
     else:
         update.message.reply_text("This group has an unfinished game!")
 
@@ -25,44 +25,47 @@ def start(update: telegram.Update, context: telegram.ext.CallbackContext):
 
 
 def join(update: telegram.Update, context: telegram.ext.CallbackContext):
-    user_info = update.message.from_user
+    user = update.message.from_user
     group_data = context.chat_data
-    if("active_game" in group_data.keys()):
+    user_data = context.user_data
+    if "active_game" in group_data.keys():
         group_game = group_data["active_game"]
-        group_game.join_game(user_info, update)
+        group_game.join_game(user, user_data, update)
     else:
         update.message.reply_text("There is no game in this group!")
 
 
-
-
 def leave(update: telegram.Update, context: telegram.ext.CallbackContext):
-    user_info = update.message.from_user
+    user = update.message.from_user
     group_data = context.chat_data
-    if("active_game" in group_data.keys()):
+    user_data = context.user_data
+    if "active_game" in group_data.keys():
         group_game = group_data["active_game"]
-        group_game.leave_game(user_info, update)
+        group_game.leave_game(user, user_data, update)
     else:
         update.message.reply_text("There is no game in this group!")
 
 
 def start_game(context: telegram.ext.CallbackContext):
-    context.bot.send_message(chat_id=context.job.context, text='Game has been started!')
-    group_data = context.chat_data
-    if("active_game" in group_data.keys()):
+    group_data = context.job.context[1]
+    if "active_game" in group_data.keys():
         game = group_data["active_game"]
         game.start_game()
         for player in game.players:
             context.bot.send_message(chat_id=player.user_id, text=player.rule)
+        context.bot.send_message(chat_id=context.job.context[0], text='Game has been started!')
+    else:
+        context.bot.send_message(chat_id=context.job.context[0], text='There is no game in this group!')
 
 
 def end_game(update: telegram.Update, context: telegram.ext.CallbackContext):
     group_data = context.chat_data
-    if("active_game" in group_data.keys()):
+    if "active_game" in group_data.keys():
         update.message.reply_text("Game ended")
         del group_data["active_game"]
     else:
         update.message.reply_text("There is no game in this group!")
+
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("new", new_game))
