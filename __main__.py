8cebd4bad2532ff4379910e8873110cfece0b273
Amirparsa-Sal <Amirparsa.s@aut.ac.ir>
2020-04-21 02:45:26 +0430

    delete games global list and games to groups dic

--- __main__.py
+++ __main__.py
@@ -7,85 +7,63 @@ updater = Updater(token='1212931959:AAHH9ViQhhhVRJBsEs9EwBv2pfkg8BMDFS4', use_co
 
 
 def new_game(update: telegram.Update, context: telegram.ext.CallbackContext):
-    global games
     group_id = update.effective_chat.id
-    has_active_game = False
-    print(group_id)
-    for game in games:
-        if (game.group_chat_id == group_id):
-            has_active_game = True
-    if (not has_active_game):
+    group_data = context.chat_data
+    if("active_game" not in group_data.keys()):
         update.message.reply_text("New game started")
         game = Game(group_id)
+        group_data["active_game"] = game
         games.append(game)
         context.job_queue.run_once(start_game, 30, context=update.message.chat_id)
     else:
-        update.message.reply_text("The game has already started")
+        update.message.reply_text("This group has an unfinished game!")
 
 
 def start(update: telegram.Update, context: telegram.ext.CallbackContext):
     update.message.reply_text("Hi!")
+    context.user_data["is_subscribed"] = True
 
 
 def join(update: telegram.Update, context: telegram.ext.CallbackContext):
-    global games
     user_info = update.message.from_user
-    group_id = update.effective_chat.id
-    has_started_game = False
-    group_game = None
-    for game in games:
-        if (game.group_chat_id == group_id):
-            has_started_game = True
-            group_game = game
-    if (has_started_game):
+    group_data = context.chat_data
+    if("active_game" in group_data.keys()):
+        group_game = group_data["active_game"]
         group_game.join_game(user_info, update)
     else:
         update.message.reply_text("There is no game in this group!")
 
 
+
+
 def leave(update: telegram.Update, context: telegram.ext.CallbackContext):
-    global games
     user_info = update.message.from_user
-    group_id = update.effective_chat.id
-    has_started_game = False
-    group_game = None
-    for game in games:
-        if (game.group_chat_id == group_id):
-            has_started_game = True
-            group_game = game
-    if (has_started_game):
+    group_data = context.chat_data
+    if("active_game" in group_data.keys()):
+        group_game = group_data["active_game"]
         group_game.leave_game(user_info, update)
     else:
         update.message.reply_text("There is no game in this group!")
 
 
 def start_game(context: telegram.ext.CallbackContext):
-    global games
     context.bot.send_message(chat_id=context.job.context, text='Game has been started!')
-    group_id = context.job.context
-    for game in games:
-        if game.group_chat_id == group_id:
-            game.start_game()
-            for player in game.players:
-                context.bot.send_message(chat_id=player.user_id, text=player.rule)
-            break
+    group_data = context.chat_data
+    if("active_game" in group_data.keys()):
+        game = group_data["active_game"]
+        game.start_game()
+        for player in game.players:
+            context.bot.send_message(chat_id=player.user_id, text=player.rule)
 
 
 def end_game(update: telegram.Update, context: telegram.ext.CallbackContext):
-    global games
-    group_id = update.effective_chat.id
-    has_active_game = False
-    for game in games:
-        if (game.group_chat_id == group_id):
-            has_active_game = True
-            update.message.reply_text("Game has been ended")
-            games.remove(game)
-            break
-    if has_active_game == False:
+    group_data = context.chat_data
+    if("active_game" in group_data.keys()):
+        update.message.reply_text("Game ended")
+        del group_data["active_game"]
+    else:
         update.message.reply_text("There is no game in this group!")
 
-
-games = []
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("new", new_game))
 dispatcher.add_handler(CommandHandler("end", end_game))
