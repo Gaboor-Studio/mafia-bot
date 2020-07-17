543d1b4752098e9e0b5d3ec6ff80b4c603ce6357
Amirparsa-Sal <Amirparsa.s@aut.ac.ir>
2020-07-18 02:16:28 +0430

    change player and game class to better structured format

--- src/Game.py
+++ src/Game.py
@@ -4,35 +4,53 @@ from Player import Player
 from telegram.ext import Updater
 from Poll import Poll
 import time
+import enum
+
+
+class GameState(enum.Enum):
+    Day = 0
+    Night = 1
 
 
 class Game:
 
     def __init__(self, group_chat_id, group_data):
-        self.votes = {}
+        self.night_votes = {"Mafia_shot": None,
+                            "Detective": None, "Doctor": None, "Sniper": None}
+        self.day_votes = {}  '''example: {"amirparsa_sal" : {"yes_votes" : ["salinaria","mohokaja"], "no_votes" : [] },
+                                          "salinaria" : {"yes_votes" : ["amirparsa_sal"], "no_votes" : [] 
+                                                                                            "}'''
         self.players = []
+        self.mafias = []
+        self.citizens = []
         self.group_chat_id = group_chat_id
         self.group_data = group_data
         self.is_started = False
-        self.state = "day"
+        self.state = GameState.Day
 
     def join_game(self, user: telegram.User, user_data: telegram.ext.CallbackContext.user_data,
                   update: telegram.Update, context: telegram.ext.CallbackContext):
         if "active_game" not in user_data.keys():
             user_data["active_game"] = self
-            player = Player(user['first_name'], user['username'], user['id'], user_data)
+            player = Player(user['first_name'],
+                            user['username'], user['id'], user_data)
             self.players.append(player)
             update.message.reply_text(self.get_list())
-            context.bot.send_message(chat_id=user['id'], text="You joined the game successfully")
+            context.bot.send_message(
+                chat_id=user['id'], text="You joined the game successfully")
             arrlist = []
             self.votes.update({'@' + player.user_name: arrlist})
         else:
             if user_data["active_game"] == self:
-                update.message.reply_text('@' + user['username'] + " has already joined the game!")
-                context.bot.send_message(chat_id=user['id'], text="You have already joined the game")
+                update.message.reply_text(
+                    '@' + user['username'] + " has already joined the game!")
+                context.bot.send_message(
+                    chat_id=user['id'], text="You have already joined the game")
             else:
-                update.message.reply_text('@' + user['username'] + " has already joined a game in another group!")
-                context.bot.send_message(chat_id=user['id'], text="You have already joined a game in another group")
+                update.message.reply_text(
+                    '@' + user['username'] + " has already joined a game in another group!")
+                context.bot.send_message(
+                    chat_id=user['id'], text="You have already joined a game in another group")
 
     def leave_game(self, user: telegram.User, user_data: telegram.ext.CallbackContext.user_data,
                    update: telegram.Update):
@@ -42,68 +60,12 @@ class Game:
                 player = self.get_player_by_user(user)
                 del self.votes['@' + player.user_name]
                 self.players.remove(player)
-                update.message.reply_text('@' + player.user_name + " successfully left the game!")
+                update.message.reply_text(
+                    '@' + player.user_name + " successfully left the game!")
             else:
                 update.message.reply_text("You are not in this game!")
         else:
             update.message.reply_text("You have not joined a game yet!")
-
-    def init(self):
-        for i in range(int(len(self.players) / 3)):
-            while True:
-                r = random.randrange(0, len(self.players), 1)
-                if self.players[r].rule == "citizen":
-                    self.players[r].rule = "mafia"
-                    break
-        while True:
-            r = random.randrange(0, len(self.players), 1)
-            if self.players[r].rule == "citizen":
-                self.players[r].rule = "police"
-                break
-        while True:
-            r = random.randrange(0, len(self.players), 1)
-            if self.players[r].rule == "citizen":
-                self.players[r].rule = "doctor"
-                break
-
-    def get_list(self):
-        players_list = ""
-        for player in self.players:
-            players_list += '@' + player.user_name + '\n'
-        return players_list
-
-    def get_player_by_user(self, user: telegram.User):
-        for player in self.players:
-            if player.user_name == user["username"]:
-                return player
-        return None
-
-    def get_player_by_id(self, id: str):
-        for player in self.players:
-            if player.user_id == id:
-                return player
-        return None
-
-    def get_player_by_user_name(self, user_name: str):
-        user_name = user_name.replace('@', "")
-        for player in self.players:
-            if player.user_name == user_name:
-                return player
-        return None
-
-    def get_alive_players(self):
-        list = []
-        for player in self.players:
-            if player.is_alive:
-                list.append(player)
-        return list
-
-    def get_dead_players(self):
-        list = []
-        for player in self.players:
-            if not player.is_alive:
-                list.append(player)
-        return list
 
     def start_game(self, context: telegram.ext.CallbackContext):
         group_data = self.group_data
@@ -112,8 +74,10 @@ class Game:
             if len(game.players) > 2:
                 game.init()
                 for player in game.players:
-                    context.bot.send_message(chat_id=player.user_id, text=player.rule)
-                context.bot.send_message(chat_id=self.group_chat_id, text='Game has been started!')
+                    context.bot.send_message(
+                        chat_id=player.user_id, text=player.rule)
+                context.bot.send_message(
+                    chat_id=self.group_chat_id, text='Game has been started!')
                 self.is_started = True
                 self.turn(context)
 
@@ -124,7 +88,8 @@ class Game:
                 self.delete_game(context)
 
         else:
-            context.bot.send_message(chat_id=self.group_chat_id, text='There is no game in this group!')
+            context.bot.send_message(
+                chat_id=self.group_chat_id, text='There is no game in this group!')
 
     def delete_game(self, context: telegram.ext.CallbackContext):
         if not self.is_started:
@@ -135,48 +100,16 @@ class Game:
             del player.user_data["active_game"]
         del self.group_data["active_game"]
 
-    def show_result(self, context: telegram.ext.CallbackContext):
-        dead = {}
-        for player in self.votes.keys():
-            dead.update({player: 0})
-        # calculate votes
-        for player, player_votes in self.votes.items():
-            player_votes_print = ""
-            for v in player_votes[:len(player_votes) - 1]:
-                player_votes_print += '@' + v + ' and'
-                dead.update({'@' + v: dead.get('@' + v) + 1})
-            if len(player_votes) != 0:
-                v = player_votes[len(player_votes) - 1]
-                player_votes_print += '@' + v
-                dead.update({'@' + v: dead.get('@' + v) + 1})
-            context.bot.send_message(chat_id=self.group_chat_id, text=player + ' voted to ' + player_votes_print)
-        # finding maxiumum votes
-        max_vote_players_list = []
-        dead_player = ""
-        dead_player_vote = 0
-        for player, num in dead.items():
-            if num > dead_player_vote:
-                dead_player_vote = num
-        for player, num in dead.items():
-            if num == dead_player_vote:
-                max_vote_players_list.append(player)
-        if len(max_vote_players_list) == 1:
-            dead_player = max_vote_players_list[0]
-            context.bot.send_message(chat_id=self.group_chat_id, text=dead_player + ' died')
-            self.get_player_by_user_name(dead_player).is_alive = False
-        else:
-            context.bot.send_message(chat_id=self.group_chat_id, text='Nobody died today')
-        for key, value in self.votes.items():
-            value.clear()
-
     def day(self, context: telegram.ext.CallbackContext):
         self.state = "day"
         for player in self.get_alive_players():
             player.talk(self.group_chat_id, context)
         for player in self.get_alive_players():
-            poll = Poll("Who do you want to kill?", self.get_alive_players(), player)
+            poll = Poll("Who do you want to kill?",
+                        self.get_alive_players(), player)
             poll.send_poll(context)
-        context.bot.send_message(chat_id=self.group_chat_id, text="30 seconds left until the end of voting")
+        context.bot.send_message(
+            chat_id=self.group_chat_id, text="30 seconds left until the end of voting")
         context.job_queue.run_once(self.show_result, 30, context)
 
     def get_mafia_number(self):
@@ -186,122 +119,3 @@ class Game:
                 counter = counter + 1
         return counter
 
-    def mafia_list(self, context):
-        mafias_list = ""
-        founded_mafias = 0
-        for player in self.players:
-            if player.rule == 'mafia':
-                founded_mafias += 1
-                if founded_mafias != self.get_mafia_number():
-                    mafias_list += "@" + player.user_name + " and "
-                if founded_mafias == self.get_mafia_number():
-                    mafias_list += "@" + player.user_name
-        message = ""
-        if self.get_mafia_number() == 1:
-            message = "You are the only mafia of the game. good luck :)"
-        if self.get_mafia_number() > 1:
-            message = "Your mafia team is " + mafias_list + "good luck :)"
-        return message
-
-    def mafia_want(self):
-        targets = []
-        for player in self.get_alive_players():
-            if player.rule != 'mafia':
-                targets.append(player)
-        return targets
-
-    def other_players(self, player):
-        players = []
-        for p in self.get_alive_players():
-            if p != player:
-                players.append(p)
-        return players
-
-    def show_night_result(self, context: telegram.ext.CallbackContext):
-        mafia_kill = ''
-        mafia_kill_result = 1
-        doctor_save = ''
-        police_choice = ''
-        police_choice_result = 0
-
-        for player, choice in self.votes.items():
-            if self.get_player_by_user_name(player).rule == 'mafia':
-                if len(choice) > 0:
-                    mafia_kill = choice[0]
-            if self.get_player_by_user_name(player).rule == 'doctor':
-                if len(choice) > 0:
-                    doctor_save = choice[0]
-            if self.get_player_by_user_name(player).rule == 'police':
-                if len(choice) > 0:
-                    police_choice = choice[0]
-        if mafia_kill == doctor_save:
-            mafia_kill_result = 0
-        if police_choice != '' and self.get_player_by_user_name(police_choice).rule == 'mafia':
-            police_choice_result = 1
-
-        if mafia_kill_result == 1:
-            self.get_player_by_user_name(mafia_kill).is_alive = False
-            context.bot.send_message(chat_id=self.group_chat_id, text='@' + mafia_kill + ' died')
-        else:
-            context.bot.send_message(chat_id=self.group_chat_id, text='Nobody died last night!')
-        if police_choice_result == 1:
-            context.bot.send_message(chat_id=self.group_chat_id, text="Police guessed right!")
-        else:
-            context.bot.send_message(chat_id=self.group_chat_id, text="Police guessed wrong!")
-        for key, value in self.votes.items():
-            value.clear()
-
-    def poll_night(self, context: telegram.ext.CallbackContext):
-        for player in self.get_alive_players():
-            if player.rule == 'police':
-                poll = Poll("who do you doubt🕵️?", self.other_players(player), player)
-                poll.send_poll(context)
-            if player.rule == 'doctor':
-                poll = Poll("who do you want to save👨‍", self.get_alive_players(), player)
-                poll.send_poll(context)
-            if player.rule == 'mafia':
-                poll = Poll("who do you want to kill😈", self.mafia_want(), player)
-                poll.send_poll(context)
-
-    def night(self, context: telegram.ext.CallbackContext):
-        self.state = "night"
-        self.poll_night(context)
-        context.bot.send_message(chat_id=self.group_chat_id, text="60 second left from night!")
-        context.job_queue.run_once(self.show_night_result, 60, context)
-
-    def end_game(self, context: telegram.ext.CallbackContext):
-        mafia = 0
-        citizen = 0
-        for player in self.get_alive_players():
-            if player.rule == 'mafia':
-                mafia = mafia + 1
-            else:
-                citizen = citizen + 1
-        if mafia >= citizen:
-            context.bot.send_sticker(chat_id=self.group_chat_id,
-                                     sticker="CAACAgQAAxkBAAEBEGpfEajpXdMaTTseiJvWttCJFXbtwwACGQAD1ul3K3z-LuYH7F5fGgQ")
-            context.bot.send_message(chat_id=self.group_chat_id, text="Mafia win!")
-            self.is_started = False
-            return False
-        elif mafia == 0:
-            context.bot.send_sticker(chat_id=self.group_chat_id,
-                                     sticker="CAACAgQAAxkBAAEBEGhfEajlbVPbMEesXXrgq4wOe-5eBAACGAAD1ul3K4WFHtFPPfm2GgQ")
-            context.bot.send_message(chat_id=self.group_chat_id, text="City win!")
-            self.is_started = False
-            return False
-        else:
-            return True
-
-    def turn(self, context: telegram.ext.CallbackContext):
-        if self.end_game(context):
-            self.day(context)
-        else:
-            return True
-        if self.end_game(context):
-            context.job_queue.run_once(self.night, 50, context)
-        else:
-            return True
-        if self.end_game(context):
-            context.job_queue.run_once(self.turn, 180, context)
-        else:
-            return True
--- src/Player.py
+++ src/Player.py
@@ -2,18 +2,153 @@ import telegram
 from Poll import Poll
 from telegram.ext import CommandHandler
 import time
+import enum
+
+
+class Rules(enum.Enum):
+    GodFather = 0
+    Mafia = 1
+    Citizen = 2
+    Doctor = 3
+    Detective = 4
+    Sniper = 5
+
+
+class Teams(enum.Enum):
+    Mafia = 0
+    City = 1
 
 
 class Player:
-    def __init__(self, name, user_name, user_id, user_data):
-        self.rule = "citizen"
+    '''Attributes:
+        active_game: player's active game
+        name: player's profile name
+        user_name: player's username
+        user_id: player's chat id
+        user_data: player's telegram data
+        is_alive: is player alive or not
+        emoji: player's emoji
+    '''
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        self.active_game = active_game
         self.name = name
         self.user_name = user_name
         self.user_id = user_id
         self.user_data = user_data
         self.is_alive = True
+        self.emoji = None
 
-    def talk(self, group_chat_id, context: telegram.ext.CallbackContext):
-        context.bot.send_message(chat_id=group_chat_id,
-                                 text='@' + self.user_name + " turn to talk")
-        time.sleep(5)
+    def send_day_poll(self):
+        pass
+
+    def doTheJob(self):
+        pass
+
+    def toString(self):
+        pass
+
+
+class Mafia(Player):
+
+    '''Additional attributes:
+        team_mates_username: list of team mates usernames
+    '''
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        super().__init__(name, user_name, user_id, user_data, active_game)
+        self.emoji = "!"
+        self.team_mates_username = []
+
+    def findTeam(self):
+        return "Mafia"
+
+    def findRule(self):
+        return Rules.Mafia
+
+    def doTheJob(self):
+        # Ask for shot
+        pass
+
+
+class GodFather(Mafia):
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        super().__init__(name, user_name, user_id, user_data, active_game)
+        self.emoji = "!!!"
+
+    def findRule(self):
+        return Rules.GodFather
+
+
+class Citizen(Player):
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        super().__init__(name, user_name, user_id, user_data, active_game)
+        self.emoji = ":)"
+
+    def findTeam(self):
+        return Teams.City
+
+    def findRule(self):
+        return Rules.Citizen
+
+
+class Doctor(Citizen):
+
+    '''Additional attributes:
+        saved_himself: number of times that doctor saved himself
+    '''
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        super().__init__(name, user_name, user_id, user_data, active_game)
+        self.emoji = "+"
+        self.saved_himself = 0
+        self.maximum_himself_save = 2
+
+    def set_maxiumum_himself_save(self, saves_number):
+        self.maximum_himself_save = saves_number
+
+    def findRule(self):
+        return Rules.Doctor
+
+
+class Detective(Citizen):
+
+    '''Detective attributes:
+        detections: a dictionary that saves previous detections
+    '''
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        super().__init__(name, user_name, user_id, user_data, active_game)
+        self.emoji = "*"
+        self.detections = {}
+
+    def findRule(self):
+        return Rules.Detective
+
+    def doTheJob(self):
+        # Send the poll for detection
+        pass
+
+
+class Sniper(Citizen):
+
+    '''Additional attributes:
+        shots: number of sniper's shots
+    '''
+
+    def __init__(self, name, user_name, user_id, user_data, active_game):
+        super().__init__(name, user_name, user_id, user_data, active_game)
+        self.emoji = "->"
+        self.shots = 1
+
+    def set_shots(self, number):
+        self.shots = number
+
+    def doTheJob(self):
+        # Send the poll
+        pass
+
+    def findRule(self):
+        return Rules.Sniper
